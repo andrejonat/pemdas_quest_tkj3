62cd09b12be4e39167b3c4b1d58e70537a13b304
andrejonat <112368139+andrejonat@users.noreply.github.com>
2022-09-05 12:09:39 +0700

    andre xtkj 3 pemdas
    tugas python 6

---
+++ python6-tkj3.py
@@ -0,0 +1,16 @@
+# dolar ke rupiah
+
+dolar = int(input("masukkan nilai dolar: "))
+nilai_r = dolar * 14920
+print("nilai 1 dolar adalah", nilai_r)
+
+# euro ke won
+
+euro = int(input("masukkan nilai euro: "))
+nilai_w = euro * 1357
+print("nilai 1 euro adalah", nilai_w)
+
+# yen ke rupiah
+yen = int(input("masukkan nilai yen: "))
+nilai_r = yen * 106
+print("nilai 1 yen adalah",nilai_r)
